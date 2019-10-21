8c108cee77a87f7a7f2f292b7f831c668794f7bc
wangwu <1843080373@qq.com>
2019-10-21 10:01:05 +0800

    jar找不到

--- spring-boot-security-oauth2-samples/spring-security-oauth2/spring-security-oauth2-server/src/main/java/com/funtl/oauth2/OAuth2ServerApplication.java
+++ spring-boot-security-oauth2-samples/spring-security-oauth2/spring-security-oauth2-server/src/main/java/com/funtl/oauth2/OAuth2ServerApplication.java
@@ -14,6 +14,11 @@ import tk.mybatis.spring.annotation.MapperScan;
 @MapperScan(basePackages = "com.funtl.oauth2.server.mapper")
 public class OAuth2ServerApplication {
 
+    /**
+     * 1.配置oauth2库的oauth_client_details表
+     * 2.去这里授权
+     * http://localhost:8070/oauth/authorize?client_id=client&response_type=code
+     */
     public static void main(String[] args) {
         SpringApplication.run(OAuth2ServerApplication.class, args);
     }
--- spring-boot-security-oauth2-samples/spring-security-oauth2/spring-security-oauth2-server/src/test/java/com/funtl/oauth2/test/BCryptPasswordEncoderTest.java
+++ spring-boot-security-oauth2-samples/spring-security-oauth2/spring-security-oauth2-server/src/test/java/com/funtl/oauth2/test/BCryptPasswordEncoderTest.java
@@ -6,7 +6,6 @@ public class BCryptPasswordEncoderTest {
 
 	public static void main(String[] args) {
 		System.out.println(new BCryptPasswordEncoder().encode("secret"));
-	
 	}
 
 }
