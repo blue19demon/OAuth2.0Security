2b8e2d1de4106b507c002fde0e94277d0440471b
wangwu <1843080373@qq.com>
2019-10-21 10:06:51 +0800

    获取token API

--- spring-boot-security-oauth2-samples/spring-security-oauth2/spring-security-oauth2-server/src/main/java/com/funtl/oauth2/OAuth2ServerApplication.java
+++ spring-boot-security-oauth2-samples/spring-security-oauth2/spring-security-oauth2-server/src/main/java/com/funtl/oauth2/OAuth2ServerApplication.java
@@ -18,6 +18,8 @@ public class OAuth2ServerApplication {
      * 1.配置oauth2库的oauth_client_details表
      * 2.去这里授权
      * http://localhost:8070/oauth/authorize?client_id=client&response_type=code
+     * 3.curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'grant_type=authorization_code&code=1JuO6V' "http://client:secret@localhost:8070/oauth/token"
+                * 去获取token
      */
     public static void main(String[] args) {
         SpringApplication.run(OAuth2ServerApplication.class, args);
